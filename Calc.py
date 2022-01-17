b9f0b84de88a41a9a040baea52f2b17c5ea4b596
Namasday <2790854408@qq.com>
2022-01-17 17:13:05 +0800

    change

--- Calc.py
+++ Calc.py
@@ -19,9 +19,7 @@ def calc_rigidbodies():
             point_number.append(j)
 
         #提取每个面对应的刚体的中心
-        loc_x = bpy.context.object.data.polygons[i].center[0]
-        loc_y = bpy.context.object.data.polygons[i].center[1]
-        loc_z = bpy.context.object.data.polygons[i].center[2]
+        loc = bpy.context.object.data.polygons[i].center
 
         #读取面的法向存入faces_normal
         faces_normal = list(bpy.context.object.data.polygons[i].normal)
@@ -29,35 +27,11 @@ def calc_rigidbodies():
         point_normal = []
         #坐标系变换
         for k in point_number:
-            point_x = bpy.context.object.data.vertices[k].co[0]
-            point_y = bpy.context.object.data.vertices[k].co[1]
-            point_z = bpy.context.object.data.vertices[k].co[2]
-            point_normal.append(exchange_axis(faces_normal,point_x,point_y,point_z))
+            point_loc = bpy.context.object.data.vertices[k].co
+            point_normal.append(exchange_axis(faces_normal,point_loc)
 
         #计算刚体尺寸
-        list_nx = []
-        list_ny = []
-        list_nz = []
 
-        for m in point_normal:
-            list_nx.append(m[0])
-            list_ny.append(m[1])
-            list_nz.append(m[2])
-
-        list_nx = sorted(list_nx)
-        list_ny = sorted(list_ny)
-        list_nz = sorted(list_nz)
-
-        max_middle_x = (list_nx[-2] + list_nx[-1]) / 2
-        min_middle_x = (list_nx[0] + list_nx[1]) / 2
-        max_middle_y = (list_ny[-2] + list_ny[-1]) / 2
-        min_middle_y = (list_ny[0] + list_ny[1]) / 2
-        max_middle_z = (list_nz[-2] + list_nz[-1]) / 2
-        min_middle_z = (list_nz[0] + list_nz[1]) / 2
-
-        size_x = (max_middle_x + min_middle_x) / 2
-        size_y = (max_middle_z + min_middle_z) / 2
-        size_z = (max_middle_y + min_middle_y) / 2
 
         #计算刚体旋转
         faces_normal = np.array(faces_normal)
@@ -73,6 +47,8 @@ def calc_rigidbodies():
 
         # 转Z
         cos_z = np.dot(world_vector, tr1) / len2
+        if cos_z > 1:
+            cos_z = 1
         if tr1[0] >= 0:
             angel_z = -np.arccos(cos_z)
         else:
@@ -81,6 +57,8 @@ def calc_rigidbodies():
 
         # 转X
         cos_x = np.dot(tr1, tr2) / len2
+        if cos_x > 1:
+            cos_x = 1
         if tr1[2] >= 0:
             angel_x = np.arccos(cos_x)
         else:
@@ -88,6 +66,9 @@ def calc_rigidbodies():
         print(angel_x)
 
         # 转Y
-        world_z = np.array([0, 0, 1])
-        tr1_x0 = np.cross(tr1, world_z)
-        tr1_z = np.cross(tr1_x0, tr1)
+        poi1 = bpy.context.object.data.vertices[point_number[-1]].co
+        poi2 = bpy.context.object.data.vertices[point_number[-2]].co
+
+        poi1 = np.array(poi1)
+        poi2 = np.array(poi2)
+
